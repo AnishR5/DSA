1f7c2448cd1af31fe3bec20fa2162575078b0b81
Anish Rachcha <anishrachcha@gmail.com>
2026-01-22 21:10:57 +0530

    selection sort

---
+++ Day4/selectionSort.js
@@ -0,0 +1,21 @@
+const selectionSort = (arr) => {
+  console.log("Original Arr : ", arr);
+  for (let i = 0; i < arr.length -1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if(arr[j]<arr[minIndex]){
+        minIndex = j;
+      }
+    }
+
+    if(minIndex !=i){
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+
+  console.log("Sorted Arr : ", arr);
+};
+
+selectionSort([13, 46, 24, 51, 20, 9]);
