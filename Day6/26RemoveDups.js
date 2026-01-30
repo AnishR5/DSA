e69ce5dbb66be88053b18f12ab5dc427b595f0d4
Anish Rachcha <anishrachcha@gmail.com>
2026-01-30 22:35:25 +0530

    array

--- Day6/26RemoveDups.js
+++ Day6/26RemoveDups.js
@@ -21,4 +21,21 @@ console.log(final);
 
 };
 
+
+//two pointer
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+
+    let k = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
+
 removeDuplicates([1,1,2])
